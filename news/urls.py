779791281e682b78f846b924580b56bf31eeeed1
Wahidul Islam <wahidulislambd4@gmail.com>
2024-10-15 22:05:15 +0600

    Initial commit

---
+++ news/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("article/", include("article.urls")),
+    path("author/", include("author.urls")),
+    path("content/", include("content.urls")),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# # urls.py
+# from django.shortcuts import render
+
+# def custom_page_not_found_view(request, exception):
+#     return render(request, "frontend/404.html", {})
+
+# handler404 = 'portfolio.urls.custom_page_not_found_view'
